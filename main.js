16ee3277e8de4ad3618644ceedfcf3f0291535eb
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-04-26 16:27:37 +0100

    Improved logic to allow for multi digit numbers

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//math functions
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -14,142 +16,96 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-//Whenever number is pressed, append it to a list. then,
-//whatever symbol is pressed is saved, so for the next number pressed,
-//it appends the list with number[-2] symbol number[-1], so the list
-//is always only 2 items long really
+//var initialisations
 
-let numbersList = [];
-let calculation;
+let mainNumber;
+let seconNumber;
+let calculation = "NOT";
+
+let displayMainNumber = "";
+let displaySeconNumber = "";
+let displayCalculation = "";
+
+//da code
 
 clearButton = document.querySelector("#buttonClear");
 
 clearButton.addEventListener("click", function () {
-  numbersList.length = 0;
-  display.textContent = numbersList[0];
+  mainNumber = null;
+  seconNumber = null;
+  calculation = "NOT";
+
+  displayMainNumber = "";
+  displaySeconNumber = "";
+  displayCalculation = "";
+
+  display.textContent = "";
 });
 
 display = document.querySelector("p");
+
+equalsButton = document.querySelector("#buttonEquals");
+
+equalsButton.addEventListener("click", (e) => {
+  if (calculation === "addition") {
+    mainNumber = add(mainNumber, seconNumber);
+    seconNumber = null;
+    displaySeconNumber = "";
+    displayMainNumber = mainNumber.toString();
+  }
+
+  display.textContent = displayMainNumber.toString();
+
+  calculation = "NOT";
+  displayCalculation = "";
+});
 
 additionButton = document.querySelector("#buttonPlus");
 
 additionButton.addEventListener("click", function () {
   calculation = "addition";
+  displayCalculation = " + ";
+  display.textContent = mainNumber
+    .toString()
+    .concat(displayCalculation, displaySeconNumber);
 });
 
-//add equals button and always perform the calculation on that
-//this way you can have numbers more than one digit long
+numberButtons = document.querySelectorAll(".buttonsNumber");
 
-button0 = document.querySelector("#button0");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (calculation === "NOT") {
+      displayCalculation = "";
+      if (mainNumber == null) {
+        mainNumber = Number(e.target.textContent);
+      } else {
+        mainNumber = Number(mainNumber.toString().concat(e.target.textContent));
+      }
+    } else if (calculation === "addition") {
+      displayCalculation = " + ";
+      if (seconNumber == null) {
+        seconNumber = Number(e.target.textContent);
+      } else {
+        seconNumber = Number(
+          seconNumber.toString().concat(e.target.textContent)
+        );
+      }
+    }
 
-/*
-  else if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }*/
+    if (mainNumber == null) {
+      displayMainNumber = "";
+    } else {
+      displayMainNumber = mainNumber.toString();
+    }
 
-button0.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "NOT") {
-  }
-  display.textContent = numbersList[0];
-});
+    if (seconNumber == null) {
+      displaySeconNumber = "";
+    } else {
+      displaySeconNumber = seconNumber.toString();
+    }
 
-button1 = document.querySelector("#button1");
-
-button1.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button2 = document.querySelector("#button2");
-
-button2.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button3 = document.querySelector("#button3");
-
-button3.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button4 = document.querySelector("#button4");
-
-button4.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button5 = document.querySelector("#button5");
-
-button5.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button6 = document.querySelector("#button6");
-
-button6.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button7 = document.querySelector("#button7");
-
-button7.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button8 = document.querySelector("#button8");
-
-button8.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
-});
-
-button9 = document.querySelector("#button9");
-
-button9.addEventListener("click", function () {
-  numbersList.push(Number(this.textContent));
-  if (calculation === "addition" && numbersList.length > 1) {
-    numbersList = [add(numbersList[0], numbersList[1])];
-    display.textContent = numbersList[0];
-  }
-  display.textContent = numbersList[0];
+    display.textContent = mainNumber
+      .toString()
+      .concat(displayCalculation, displaySeconNumber);
+  });
 });
